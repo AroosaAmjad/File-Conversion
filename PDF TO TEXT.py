8b731978050a04508eb2b4f2d824cea6f7fbd39e
AroosaAmjad <116438299+AroosaAmjad@users.noreply.github.com>
2024-03-04 23:25:53 +0500

    Add files via upload

--- PDF TO TEXT.py
+++ PDF TO TEXT.py
@@ -1,36 +1,3 @@
-# import tkinter as tk
-# from tkinter import filedialog, messagebox
-# import fitz  # PyMuPDF
-
-# def pdf_to_text():
-#     file_path = filedialog.askopenfilename()
-#     if file_path:
-#         try:
-#             text = ''
-#             with fitz.open(file_path) as pdf_file:
-#                 for page_num in range(len(pdf_file)):
-#                     page = pdf_file[page_num]
-#                     text += page.get_text()
-#             text_output.delete('1.0', tk.END)
-#             text_output.insert(tk.END, text)
-#         except Exception as e:
-#             messagebox.showerror("Error", f"An error occurred: {str(e)}")
-
-# # Create main window
-# root = tk.Tk()
-# root.title("PDF to Text Converter")
-
-# # Create button to open PDF file
-# open_button = tk.Button(root, text="Open PDF", command=pdf_to_text)
-# open_button.pack(pady=10)
-
-# # Create text widget to display the extracted text
-# text_output = tk.Text(root, height=20, width=50)
-# text_output.pack(padx=10, pady=10)
-
-# # Run the main event loop
-# root.mainloop()
-
 import tkinter as tk
 from tkinter import filedialog, messagebox
 import fitz  # PyMuPDF
